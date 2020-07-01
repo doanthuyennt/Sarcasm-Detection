769ee9d49b86bf7c194c60e94b2701fe3ba41680
Tran Doan Thuyen <thuyentrandoan.ttd@gmail.com>
2020-07-01 15:16:22 +0700

    add get drive file utils

---
+++ utils/get_drive_file.py
@@ -0,0 +1,12 @@
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+	
+def get_drive_file(ID,file_name):
+	# For example: "1-sltCi9nCSQmA-xaBPutq6Yfl6YONC_H" id of Tokenizer file
+	auth.authenticate_user()
+	gauth = GoogleAuth()
+	gauth.credentials = GoogleCredentials.get_application_default()
+	drive = GoogleDrive(gauth)
+	file_id = ID
+	downloaded = drive.CreateFile({'id': file_id})
+	downloaded.GetContentFile(os.path.join(data_dir, file_name))
